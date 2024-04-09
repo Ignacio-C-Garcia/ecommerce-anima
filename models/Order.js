171253972623227b8817115790482f6d3f9e17a0
JeanFranco60 <jeansueiro60@gmail.com>
2024-04-09 20:34:35 -0300

    order model added

---
+++ models/Order.js
@@ -0,0 +1,30 @@
+const { Model, DataTypes } = require("sequelize");
+
+class Order extends Model {
+  static initModel(sequelize) {
+    Order.init(
+      {
+        id: {
+          type: DataTypes.BIGINT,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        productList: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        status: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+      },
+      {
+        sequelize,
+        modelName: "order",
+      }
+    );
+    return Order;
+  }
+}
+
+module.exports = Order;
