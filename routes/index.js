9998a6550acb7e7008e3a078176a609bbd1190db
Ichiro The Cringy <agustingabrielgarcia@outlook.com>
2024-04-09 20:51:24 -0300

    line 5 fixed

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const adminRoutes = require("./adminRoutes");
 
-router.use("/admins", Routes);
+router.use("/admins", adminRoutes);
 
 module.exports = router;
