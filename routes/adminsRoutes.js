9215ffa0ac9d1f86a62bb378fb4b056ad77218e4
Ichiro The Cringy <agustingabrielgarcia@outlook.com>
2024-04-09 20:17:24 -0300

    routes folder created, index file created, admin routes created

---
+++ routes/adminsRoutes.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const router = express.Router();
+const { Admin } = require("../models");
+
+router.post("/", async (req, res) => {
+  const { surname, name, email, password } = req.body;
+  await Admin.create({ surname, name, email, password });
+  return res.send("Register added successfully!");
+});
+
+router.get("/", async (req, res) => {
+  const admins = await Admin.findAll();
+  return res.json(admins);
+});
+
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const admin = await Admin.findByPk(id);
+  return res.json(admin);
+});
+
+router.patch("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { surname, name, email, password } = req.body;
+
+  const admin = await Admin.findByPk(id);
+
+  if (name) admin.name = name;
+  if (surname) admin.surname = surname;
+  if (email) admin.email = email;
+  if (password) admin.password = password;
+
+  await admin.save();
+
+  return res.send("Admin modified successfully!");
+});
+
+module.exports = router;
