1e9fa2e3c39362132293dc10b4410b23d8f4c509
Ichiro The Cringy <agustingabrielgarcia@outlook.com>
2024-04-09 20:50:30 -0300

    routes.js deleted

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const adminRoutes = require("./adminRoutes");
 
-router.use("/admins", adminRoutes);
+router.use("/admins", Routes);
 
 module.exports = router;
