66c621dea2218ae67c1c6a12c5074b1502f2fdcf
Ichiro The Cringy <130695643+ichirothecringy-dev@users.noreply.github.com>
2024-04-10 18:53:44 -0300

    admin controller and routes fixed

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,9 +1,37 @@
+const { Admin } = require("../models");
+
 const adminController = {
-  // index: async(req,res)=>{}
-  // show: 
-  // create:
-  // update:
-  // destroy:
+  index: async (req, res) => {
+    const admins = await Admin.findAll();
+    return res.json(admins);
+  },
+  show: async (req, res) => {
+    const { id } = req.params;
+    const admin = await Admin.findByPk(id);
+    return res.json(admin);
+  },
+  store: async (req, res) => {
+    const { surname, name, email, password } = req.body;
+    await Admin.create({ surname, name, email, password });
+    return res.send("Register added successfully!");
+  },
+  update: async (req, res) => {
+    const { id } = req.params;
+    const { surname, name, email, password } = req.body;
+
+    const admin = await Admin.findByPk(id);
+
+    if (name) admin.name = name;
+    if (surname) admin.surname = surname;
+    if (email) admin.email = email;
+    if (password) admin.password = password;
+
+    await admin.save();
+
+    return res.send("Admin modified successfully!");
+  },
+  destroy: async (req, res) => {},
+  // TODO: Write the destroy method
 };
 
 module.exports = adminController;
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,41 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { Admin } = require("../models");
+const adminController = require("../controllers/adminController");
 
-router.post("/", async (req, res) => {
-  const { surname, name, email, password } = req.body;
-  await Admin.create({ surname, name, email, password });
-  return res.send("Register added successfully!");
-});
-
-router.get("/", async (req, res) => {
-  const admins = await Admin.findAll();
-  return res.json(admins);
-});
-
-router.get("/:id", async (req, res) => {
-  const { id } = req.params;
-  const admin = await Admin.findByPk(id);
-  return res.json(admin);
-});
-
-router.patch("/:id", async (req, res) => {
-  const { id } = req.params;
-  const { surname, name, email, password } = req.body;
-
-  const admin = await Admin.findByPk(id);
-
-  if (name) admin.name = name;
-  if (surname) admin.surname = surname;
-  if (email) admin.email = email;
-  if (password) admin.password = password;
-
-  await admin.save();
-
-  return res.send("Admin modified successfully!");
-});
-
-// router.get("/", userController.index);
-
+router.get("/", adminController.index);
+router.get("/id", adminController.show);
+router.post("/", adminController.store);
+router.patch("/:id", adminController.update);
+router.delete("/:id", adminController.destroy);
 
 module.exports = router;
