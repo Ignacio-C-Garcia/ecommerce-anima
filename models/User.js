cdb5ed9774963ae982a52b43058a3859aeac6a44
almidon07 <pallison931@gmail.com>
2024-04-10 18:50:10 -0300

    User model created

---
+++ models/User.js
@@ -0,0 +1,45 @@
+const { Model, DataTypes } = require("sequelize");
+
+class User extends Model {
+  static initModel(sequelize) {
+    User.init(
+      {
+        id: {
+          type: DataTypes.BIGINT,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        name: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        surname: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        email: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        address: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        phone: {
+          type: DataType.STRING,
+          allowNull: false,
+        },
+        password: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+      },
+      {
+        sequelize,
+        modelName: "user",
+      }
+    );
+    return User;
+  }
+}
+module.exports = User;
