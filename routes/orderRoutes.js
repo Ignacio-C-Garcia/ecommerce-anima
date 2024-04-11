d7f1dab1b5465d6b7fc6e9dfaafbb139eb2537cd
JeanFranco60 <jeansueiro60@gmail.com>
2024-04-11 20:16:41 -0300

    destroy method added and order controller added in routes

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,36 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { Order } = require("../models");
+const orderController = require("../controllers/orderController");
 
-router.post("/", async (req, res) => {
-  const { productList, status } = req.body;
-  await Order.create({ productList, status });
-  return res.send("Register added successfully!");
-});
-
-router.get("/", async (req, res) => {
-  const orders = await Order.findAll();
-  return res.json(orders);
-});
-
-router.get("/:id", async (req, res) => {
-  const { id } = req.params;
-  const order = await Order.findByPk(id);
-  return res.json(order);
-});
-
-router.patch("/:id", async (req, res) => {
-  const { id } = req.params;
-  const { productList, status } = req.body;
-
-  const admin = await Admin.findByPk(id);
-
-  if (status) order.status = status;
-  if (productList) admin.productList = productList;
-
-  await order.save();
-
-  return res.send("Admin modified successfully!");
-});
+router.get("/", orderController.index);
+router.get("/:id", orderController.show);
+router.post("/", orderController.store);
+router.patch("/:id", orderController.update);
+router.delete("/:id", orderController.destroy);
 
 module.exports = router;
