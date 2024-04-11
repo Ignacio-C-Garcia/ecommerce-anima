2e9bcdd249b03042610fa2f3834f5867e18eb827
Ichiro The Cringy <130695643+ichirothecringy-dev@users.noreply.github.com>
2024-04-11 18:58:39 -0300

    product and categories entities added in both index

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,14 @@ const express = require("express");
 const router = express.Router();
 const adminRoutes = require("./adminRoutes");
 const orderRoutes = require("./orderRoutes");
+const userRoutes = require("./userRoutes");
+// const productRoutes = require("/productRoutes");
+// const categoryRoutes = require("/categoryRoutes");
 
 router.use("/admins", adminRoutes);
 router.use("/orders", orderRoutes);
+router.use("/users", userRoutes);
+// router.use("/routes", productRoutes);
+// router.use("/categories", categoryRoutes);
 
 module.exports = router;
