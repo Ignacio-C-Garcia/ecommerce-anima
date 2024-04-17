23d4d73480508ddff95a4b38be60867235e3eda2
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2024-04-16 21:12:02 -0300

    funciona server y tests

--- server.js
+++ server.js
@@ -1,13 +1,5 @@
 require("dotenv").config();
-const express = require("express");
-const app = express();
-const routes = require("./routes");
-const port = process.env.APP_PORT;
-
-app.use(express.json());
-app.use(routes);
-module.exports = app;
-/*app.listen(port, () =>
+const app = require("./index");
+app.listen(process.env.APP_PORT, () =>
   console.log(`Servidor corriendo en http://${process.env.APP_DOMAIN}.\n`)
 );
-*/
--- tests/products.test.js
+++ tests/products.test.js
@@ -1,17 +1,12 @@
 const request = require("supertest");
 require("dotenv").config();
 const express = require("express");
-const app = express();
-
-app.get("/products/", (req, res) => {
-  res.json([]);
-});
+const app = require("../index");
 describe("GET /api/product/get", () => {
   it("should return all products", async () => {
     const response = await request(app).get("/products/").send();
     expect(response.statusCode).toBe(200);
     expect(response.type).toBe("application/json");
     console.log(response.text);
-  });
+  }, 10000);
 });
-
