6e99e1a8809570b9a0ecfd19bbb59194a445eb01
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2024-04-23 19:11:16 -0300

    user with hashed password

--- createTables.js
+++ createTables.js
@@ -4,6 +4,7 @@ const { sequelize } = require("./models");
 async function createTables() {
   await sequelize.sync({ force: true });
   console.log("The tables has been created successfully.");
+  process.exit();
 }
 
 createTables();
