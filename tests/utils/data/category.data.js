53efb1f6ea9eae6488c9269f0651c303a5c9e06b
ignacio <ignacioacgarcia@gmail.com>
2024-04-23 13:29:12 -0300

    category tests added

---
+++ tests/utils/data/category.data.js
@@ -0,0 +1,9 @@
+const category1 = { name: "salado" };
+
+const category2 = { name: "dulce" };
+
+const category3 = { name: "refresco" };
+
+const category4 = { name: "alcohol" };
+
+module.exports = { category1, category2, category3, category4 };
