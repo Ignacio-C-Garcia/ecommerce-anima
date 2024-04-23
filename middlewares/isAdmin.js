256f12cbec590c3d2557660089dcd8e17a88bc91
ignacio <ignacioacgarcia@gmail.com>
2024-04-23 13:28:58 -0300

    is admin updated

--- middlewares/isAdmin.js
+++ middlewares/isAdmin.js
@@ -2,7 +2,7 @@ function isAdmin(req, res, next) {
   if (req.auth.role === "Admin") {
     return next();
   }
-  res.status(401).send("Access denied. Only admins authorized.");
+  res.status(401).json({ errors: ["Access denied. Only admins authorized."] });
 }
 
 module.exports = isAdmin;
