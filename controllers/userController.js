d37eb6a76f660b0451f729694c465df040cae506
Ichiro The Cringy <130695643+ichirothecringy-dev@users.noreply.github.com>
2024-04-23 20:10:56 -0300

    change errors to json

--- controllers/userController.js
+++ controllers/userController.js
@@ -22,12 +22,14 @@ const userController = {
       !user.phone ||
       !user.password
     ) {
-      return res.status(400).send("All fields are required for user creation.");
+      return res
+        .status(400)
+        .json({ errors: ["All fields are required for user creation."] });
     }
     const hashedPassword = await bcrypt.hash(user.password, 10);
     user.password = hashedPassword;
     await User.create(user);
-    return res.send("New user has been added successfully.");
+    return res.json(user);
   },
   update: async (req, res) => {
     const { id } = req.params;
@@ -37,12 +39,12 @@ const userController = {
     if (id !== loggedInUserId) {
       return res
         .status(403)
-        .send("No tienes permiso para modificar este usuario.");
+        .json({ errors: ["No tienes permiso para modificar este usuario."] });
     }
 
     const user = await User.findByPk(id);
 
-    if (!user) res.status(404).send("Admin not found.");
+    if (!user) res.status(404).json({ errors: ["Admin not found."] });
     if (userInfo.name) user.name = userInfo.name;
     if (userInfo.surname) user.surname = userInfo.surname;
     if (userInfo.email) user.email = userInfo.email;
@@ -54,19 +56,19 @@ const userController = {
     }
 
     await user.save();
-    return res.send("User has been modified successfully.");
+    return res.json(user);
   },
   destroy: async (req, res) => {
     const { id } = req.params;
     try {
       const user = await User.findByPk(id);
       if (!user) {
-        return res.status(404).send("User not found.");
+        return res.status(404).json({ errors: ["User not found."] });
       }
       user.destroy();
-      return res.send("The user has been deleted successfully.");
+      return res.json(user);
     } catch (err) {
-      return res.send(err);
+      return res.json(err);
     }
   },
 };
