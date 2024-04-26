86f3d18aecbd7c723c9980687144896a02a415b3
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2024-04-26 18:59:47 -0300

    categoryroutes checkjwt and isadmin added

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const categoryController = require("../controllers/categoryController");
+const isAdmin = require("../middlewares/isAdmin");
+const checkjwt = require("../middlewares/checkjwt");
 
 router.get("/", categoryController.index);
 router.get("/:id", categoryController.show);
+
+router.use(checkjwt, isAdmin);
+
 router.post("/", categoryController.store);
 router.patch("/:id", categoryController.update);
 router.delete("/:id", categoryController.destroy);
