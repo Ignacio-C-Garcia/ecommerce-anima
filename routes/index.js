c28a03ca8bf64babbe0501db7a23a6184ccc768a
ignacio <ignacioacgarcia@gmail.com>
2024-07-06 17:07:21 -0300

    actualizado para utilizar react stripe

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-
+const path = require("path");
 const adminRoutes = require("./adminRoutes");
 const userRoutes = require("./userRoutes");
 const orderRoutes = require("./orderRoutes");
@@ -9,6 +9,8 @@ const categoryRoutes = require("./categoryRoutes");
 const authRoutes = require("./authRoutes");
 const kittenRoutes = require("./kittenRoutes");
 
+const stripeRoutes = require("./stripeRoutes");
+
 router.use("/admins", adminRoutes);
 router.use("/users", userRoutes);
 router.use("/orders", orderRoutes);
@@ -16,7 +18,25 @@ router.use("/products", productRoutes);
 router.use("/categories", categoryRoutes);
 router.use("/tokens", authRoutes);
 router.use("/kittens", kittenRoutes);
+router.use("/stripe", stripeRoutes);
+console.log(__dirname);
+router.use(
+  "/.well-known/apple-developer-merchantid-domain-association",
+  (req, res) => {
+    const filePath = path.join(
+      __dirname,
+      "apple-developer-merchantid-domain-association"
+    );
+    res.sendFile(filePath, (err) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Error al enviar el archivo");
+      }
+    });
+  }
+);
 router.use(function (req, res) {
   res.status(404).json({ errors: ["Endpoint not found"] });
 });
+
 module.exports = router;
