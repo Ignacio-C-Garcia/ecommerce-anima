01bee87bf77b68f40dcb68b39ccf208f2a86893e
ignacio <ignacioacgarcia@gmail.com>
2024-07-10 14:49:25 -0300

    kittenSeeder modificado

--- seeders/kittenSeeder.js
+++ seeders/kittenSeeder.js
@@ -1,62 +1,61 @@
-const { logger } = require("sequelize/lib/utils/logger");
 const { Kitten } = require("../models");
 module.exports = async () => {
   const kittensList = [
     {
       pic: "venus.jpeg",
       name: "Venus",
-      birthdate: "8 de agosto",
+      birthdate: "08/08",
       age: 2,
       personality: "Curiosa y juguetona",
     },
     {
       pic: "lisa.jpg",
       name: "Lisa",
-      birthdate: "27 de abril",
+      birthdate: "27/04",
       age: 2,
       personality: "Curiosa y juguetona",
     },
     {
       pic: "luu.jpg",
       name: "Luu",
-      birthdate: "01 de octubre",
+      birthdate: "01/10",
       age: 3,
       personality: "Tímida pero cariñosa",
     },
     {
       pic: "sushi.jpg",
       name: "Sushi",
-      birthdate: "8 de abril",
+      birthdate: "08/04",
       age: 1,
       personality: "Jugetón",
     },
     {
       pic: "bombon.jpg",
       name: "Bombón",
-      birthdate: "6 de diciembre",
+      birthdate: "17/12",
       age: 1,
       personality: "cariñoso y tranquilx",
     },
     {
       pic: "manito.jpg",
       name: "Manito",
-      birthdate: "6 de diciembre",
+      birthdate: "06/12",
       age: 1,
       personality: "Dormilón",
     },
     {
       pic: "nala.jpg",
       name: "Nala",
-      birthdate: "15 de enero",
+      birthdate: "15/01",
       age: 1,
-      personality: "Juguetona y mimosa"
+      personality: "Juguetona y mimosa",
     },
     {
       pic: "Amy.jpg",
       name: "Amy",
-      birthdate: "Ni idea cuando cumple",
+      birthdate: "06/07",
       age: 6,
-      personality: "Muerde si no le das de comer"
+      personality: "Pasivo agresiva",
     },
   ];
 
