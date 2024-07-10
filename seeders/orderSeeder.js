82bdc678ac035d38cb8777d687e0555cf1da3bff
ignacio <ignacioacgarcia@gmail.com>
2024-07-10 14:49:49 -0300

    seeder de orders fixed

--- seeders/orderSeeder.js
+++ seeders/orderSeeder.js
@@ -10,16 +10,23 @@ module.exports = async function orderSeeders() {
   for (let i = 0; i < 29; i++) {
     const address = faker.location.street();
     const status = ["pendiente", "entregado", "enviado", "cancelado"];
+    const volume = ["base", 350, 450];
     const products = [];
 
     for (let j = 0; j < 3; j++) {
       const randomIndex = Math.floor(Math.random() * allProducts.length);
       const selectedProduct = allProducts[randomIndex];
+      const selectedVolume =
+        selectedProduct.categoryId === 1
+          ? volume[Math.floor(Math.random() * volume.length)]
+          : "base";
       products.push({
         id: selectedProduct.id,
         name: selectedProduct.name,
         qty: Math.floor(Math.random() * 10) + 1,
         price: selectedProduct.price,
+        volume: selectedVolume,
+        isToGo: "aqui",
       });
     }
 
