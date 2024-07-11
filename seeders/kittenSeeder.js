eff8d094603a4d152b91adca4c4b74f199724dbd
Jean <jeansueiro60@gmail.com>
2024-07-11 01:30:58 -0300

    Revert "kittenSeeder modificado"
    This reverts commit 01bee87bf77b68f40dcb68b39ccf208f2a86893e.

--- seeders/kittenSeeder.js
+++ seeders/kittenSeeder.js
@@ -1,61 +1,62 @@
+const { logger } = require("sequelize/lib/utils/logger");
 const { Kitten } = require("../models");
 module.exports = async () => {
   const kittensList = [
     {
       pic: "venus.jpeg",
       name: "Venus",
-      birthdate: "08/08",
+      birthdate: "8 de agosto",
       age: 2,
       personality: "Curiosa y juguetona",
     },
     {
       pic: "lisa.jpg",
       name: "Lisa",
-      birthdate: "27/04",
+      birthdate: "27 de abril",
       age: 2,
       personality: "Curiosa y juguetona",
     },
     {
       pic: "luu.jpg",
       name: "Luu",
-      birthdate: "01/10",
+      birthdate: "01 de octubre",
       age: 3,
       personality: "Tímida pero cariñosa",
     },
     {
       pic: "sushi.jpg",
       name: "Sushi",
-      birthdate: "08/04",
+      birthdate: "8 de abril",
       age: 1,
       personality: "Jugetón",
     },
     {
       pic: "bombon.jpg",
       name: "Bombón",
-      birthdate: "17/12",
+      birthdate: "6 de diciembre",
       age: 1,
       personality: "cariñoso y tranquilx",
     },
     {
       pic: "manito.jpg",
       name: "Manito",
-      birthdate: "06/12",
+      birthdate: "6 de diciembre",
       age: 1,
       personality: "Dormilón",
     },
     {
       pic: "nala.jpg",
       name: "Nala",
-      birthdate: "15/01",
+      birthdate: "15 de enero",
       age: 1,
-      personality: "Juguetona y mimosa",
+      personality: "Juguetona y mimosa"
     },
     {
       pic: "Amy.jpg",
       name: "Amy",
-      birthdate: "06/07",
+      birthdate: "Ni idea cuando cumple",
       age: 6,
-      personality: "Pasivo agresiva",
+      personality: "Muerde si no le das de comer"
     },
   ];
 
