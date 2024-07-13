aab68b409fff928d7d396d02d8d4ef78bea7ee43
Ichiro <130695643+ichirothecringy-dev@users.noreply.github.com>
2024-07-13 00:01:11 -0300

    first try to deploy in supabase

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ const routes = require("./routes");
 const jwtErrorHandler = require("./utils/jwtErrorHandler");
 const cors = require("cors");
 
+const sequelizeOptions = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: process.env.DB_CONNECTION,
+  logging: false,
+};
+
+if (process.env.DB_CONNECTION === "postgres") {
+  sequelizeOptions.dialectModule = require("pg");
+}
+
 app.use(cors());
 app.use(express.json());
 
