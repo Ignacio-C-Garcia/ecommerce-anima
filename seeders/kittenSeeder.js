e7586df0c0ce88dd5a59469a1c3cc9449a20ef2f
ignacio <ignacioacgarcia@gmail.com>
2024-07-14 03:09:19 -0300

    gatitus

--- seeders/kittenSeeder.js
+++ seeders/kittenSeeder.js
@@ -48,7 +48,7 @@ module.exports = async () => {
       pic: "nala.jpg",
       name: "Nala",
       birthdate: "15 de enero",
-      age: "2",
+      age: 2,
       personality: "Juguetona y mimosa",
     },
     {
